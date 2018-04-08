ea81bb6539e422e12caae1b2c18d0a26fdf800fa
qq513932815 <513932815@qq.com>
2018-04-08 17:27:44 +0800

    前后台商品管理打通-待修复商品缩略图显示bug

--- application/api/controller/ProductController.php
+++ application/api/controller/ProductController.php
@@ -10,12 +10,31 @@ namespace app\api\controller;
 
 
 use think\Controller;
+use app\api\model\Product;
 
 class ProductController extends Controller
 {
     public function listAction()
     {
-        $data = ['success' => 'ok','msg' => '666'];
-        return json($data);
+        $product = new Product();
+        //首页要加载商上架的前四条数据
+        $limit = input('limit',4);
+        $type = input('type','all');
+        switch ($type)
+        {
+            case 'new':
+                $status = 1;
+                break;
+            case 'old':
+                $status = 0;
+                break;
+            default:
+                    break;
+        }
+        $status = 1;
+        $result = $product->where([
+            'status' => $status
+        ])->paginate($limit);
+        return json($result);
     }
 }
---
+++ application/api/model/Product.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LXT
+ * Date: 2018/4/8
+ * Time: 14:48
+ */
+
+namespace app\api\model;
+
+
+use think\Model;
+
+class Product extends Model
+{
+
+}

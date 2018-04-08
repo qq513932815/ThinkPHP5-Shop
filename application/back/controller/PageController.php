3e7c6d59d2f692ce3de5d0faab2d7fece84ccd25
qq513932815 <513932815@qq.com>
2018-04-08 10:34:05 +0800

    准备开始生成静态页面

---
+++ application/back/controller/PageController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LXT
+ * Date: 2018/4/8
+ * Time: 10:21
+ */
+
+namespace app\back\controller;
+
+
+use think\Controller;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        //通过后台生成静态页面
+
+        $content = $this->fetch();
+    }
+}
